a9bc2d9a59e966b6718b204cfd617d8b7554cdba
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-12-23 18:40:14 +0900

    Refactor: 조이스틱 - 함수 분리 및 변수명 변경

--- 03.programmers-Lv2/001-조이스틱.ts
+++ 03.programmers-Lv2/001-조이스틱.ts
@@ -1,32 +1,31 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42860
 
-function solution(name: string) {
-    let answer = 0;
-    const min = 65;
-    const max = 90 + 1;
+function findChar(str: string) {
+    const min = 'A'.charCodeAt(0);
+    const max = 'Z'.charCodeAt(0) + 1;
+    const code = str.charCodeAt(0);
+    return Math.min(code - min, max - code);
+}
 
-    const findChar = (find: string) => {
-        const findCode = find.charCodeAt(0);
-        return Math.min(findCode - min, max - findCode);
-    };
-
+export function solution(name: string) {
     const chars = name.split('').map(findChar);
-    let minMove = chars.length - 1;
+
+    let chageCount = 0;
+    let minCursorCount = chars.length - 1;
 
     for (const [i, char] of chars.entries()) {
-        answer += char;
-        let cursor = i + 1;
+        chageCount += char;
+        let cursorCount = i + 1;
 
-        while (cursor < chars.length && chars[cursor] === 0) cursor++;
+        while (cursorCount < chars.length && chars[cursorCount] === 0)
+            cursorCount++;
 
-        minMove = Math.min(
-            minMove, //정방향으로 전진
-            i * 2 + chars.length - cursor, //뒤로 돌아가기
-            i + 2 * (chars.length - cursor), //역방향
+        minCursorCount = Math.min(
+            minCursorCount, //마지막 문자열까지 정방향 이동
+            i * 2 + chars.length - cursorCount, //현재 문자열까지 이동(i) + 그만큼 돌아가기(i) + 역방향으로 A를 만나기까지 움직임
+            i + 2 * (chars.length - cursorCount), //왼쪽으로 A를 만날 때까지 움직임 + 그만큼 뒤로 + 현재 문자열까지 이동(i)
         );
     }
 
-    return answer + minMove;
+    return chageCount + minCursorCount;
 }
-
-console.log(solution('BAABAA'));
