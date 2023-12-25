027822c5c2899a01b28b8a1e2f345f227329edb1
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-12-26 02:03:11 +0900

    Refactor: 피드백 기반 코드 개선

--- 01.programmers-Lv0/050-qrCode.ts
+++ 01.programmers-Lv0/050-qrCode.ts
@@ -1,9 +1,9 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/181903
 
 export function solution(q: number, r: number, code: string) {
-    return Array.from(code)
-        .map((v, i) => {
-            if (i % q === r) return v;
-        })
-        .join('');
+    const answer: string[] = [];
+    for (const [i, char] of [...code].entries()) {
+        if (i % q === r) answer.push(char);
+    }
+    return answer.join('');
 }
--- 03.programmers-Lv2/004-의상.ts
+++ 03.programmers-Lv2/004-의상.ts
@@ -1,22 +1,20 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42578
 
 type Closet = {
-    [key: string]: string[];
+    [key: string]: number;
 };
 
 export function solution(clothes: string[][]) {
     const closet: Closet = {};
-    for (const [name, category] of clothes) {
-        if (closet[category]) {
-            closet[category] = [...closet[category], name];
-        } else closet[category] = [name];
+    for (const [, category] of clothes) {
+        if (!closet[category]) closet[category] = 1;
+        else closet[category] += 1;
     }
 
     let sum = 1;
-    for (const [, name] of Object.entries(closet)) {
-        sum *= name.length + 1; //입거나 안입거나
+    for (const category in closet) {
+        sum *= closet[category] + 1; //입거나 안입거나
     }
-
     sum -= 1; //전부 안입는 경우
     return sum;
 }
