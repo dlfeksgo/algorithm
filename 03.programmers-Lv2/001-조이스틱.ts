0a0252ce05e99c75a87d299329d2a48e290078b6
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-12-25 18:38:26 +0900

    Refactor: changeCount 반복문 분리 & reverseMove 분리

--- 03.programmers-Lv2/001-조이스틱.ts
+++ 03.programmers-Lv2/001-조이스틱.ts
@@ -9,21 +9,21 @@ function findChar(str: string) {
 
 export function solution(name: string) {
     const chars = name.split('').map(findChar);
+    const chageCount = chars.reduce((a, b) => a + b);
 
-    let chageCount = 0;
     let minCursorCount = chars.length - 1;
 
-    for (const [i, char] of chars.entries()) {
-        chageCount += char;
-        let cursorCount = i + 1;
+    for (const strLength of chars.keys()) {
+        let cursorCount = strLength + 1;
 
         while (cursorCount < chars.length && chars[cursorCount] === 0)
             cursorCount++;
 
+        const reverseMove = chars.length - cursorCount;
         minCursorCount = Math.min(
             minCursorCount, //마지막 문자열까지 정방향 이동
-            i * 2 + chars.length - cursorCount, //현재 문자열까지 이동(i) + 그만큼 돌아가기(i) + 역방향으로 A를 만나기까지 움직임
-            i + 2 * (chars.length - cursorCount), //왼쪽으로 A를 만날 때까지 움직임 + 그만큼 뒤로 + 현재 문자열까지 이동(i)
+            strLength * 2 + reverseMove, //현재 문자열까지 이동(i) + 그만큼 돌아가기(i) + 역방향으로 A를 만나기까지 움직임
+            strLength + 2 * reverseMove, //왼쪽으로 A를 만날 때까지 움직임 + 그만큼 뒤로 + 현재 문자열까지 이동(i)
         );
     }
 
