e31d51bd5342f238a5dbb1ffd4a333ec9d86c620
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-12-25 20:27:46 +0900

    Refactor: 변수명 수정

--- 03.programmers-Lv2/001-조이스틱.ts
+++ 03.programmers-Lv2/001-조이스틱.ts
@@ -1,6 +1,6 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42860
 
-function findChar(str: string) {
+function getCharDistance(str: string) {
     const min = 'A'.charCodeAt(0);
     const max = 'Z'.charCodeAt(0) + 1;
     const code = str.charCodeAt(0);
@@ -8,22 +8,22 @@ function findChar(str: string) {
 }
 
 export function solution(name: string) {
-    const chars = name.split('').map(findChar);
-    const chageCount = chars.reduce((a, b) => a + b);
+    const distances = name.split('').map(getCharDistance);
+    const chageCount = distances.reduce((a, b) => a + b);
 
-    let minCursorCount = chars.length - 1;
+    let minCursorCount = distances.length - 1;
 
-    for (const strLength of chars.keys()) {
-        let cursorCount = strLength + 1;
+    for (const charIdx of distances.keys()) {
+        let cursorCount = charIdx + 1;
 
-        while (cursorCount < chars.length && chars[cursorCount] === 0)
+        while (cursorCount < distances.length && distances[cursorCount] === 0)
             cursorCount++;
 
-        const reverseMove = chars.length - cursorCount;
+        const rest = distances.length - cursorCount;
         minCursorCount = Math.min(
-            minCursorCount, //마지막 문자열까지 정방향 이동
-            strLength * 2 + reverseMove, //현재 문자열까지 이동(i) + 그만큼 돌아가기(i) + 역방향으로 A를 만나기까지 움직임
-            strLength + 2 * reverseMove, //왼쪽으로 A를 만날 때까지 움직임 + 그만큼 뒤로 + 현재 문자열까지 이동(i)
+            minCursorCount, //정방향 이동
+            charIdx * 2 + rest, //현재 문자까지 이동 거리 + 그만큼 돌아가기 + A를 제외한 나머지 문자 길이
+            charIdx + 2 * rest, //A를 제외한 나머지 문자 길이로 역방향 이동 + 그만큼 돌아가기 + 현재 문자까지 이동 거리
         );
     }
 
