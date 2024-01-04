2bebfbaf0525683f7a51c196a8b66579f4410b27
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-04 23:15:01 +0900

    Add: [Lv.1] 1문제 풀이

---
+++ 02.programmers-Lv1/005-공원산책.ts
@@ -0,0 +1,48 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/172928
+
+type Direction = {
+    [key: string]: [number, number];
+};
+
+export function solution(park: string[], routes: string[]) {
+    let [x, y]: number[] = [];
+    let maxX = park.length - 1;
+    let maxY = park[0].length - 1;
+
+    const direction: Direction = {
+        N: [-1, 0],
+        S: [1, 0],
+        W: [0, -1],
+        E: [0, 1],
+    };
+
+    for (const [i, v] of park.entries()) {
+        const start = v.indexOf('S');
+        if (start !== -1) [x, y] = [i, start];
+    }
+
+    const canMove = (x: number, y: number) => {
+        if (x < 0 || x > maxX || y < 0 || y > maxY || park[x][y] === 'X')
+            return false;
+        return true;
+    };
+
+    for (const route of routes) {
+        const [op, n] = route.split(' ');
+        let [tempX, tempY] = [x, y];
+        let flag = true;
+
+        for (let i = 0; i < Number(n); i++) {
+            tempX += direction[op][0];
+            tempY += direction[op][1];
+            if (!canMove(tempX, tempY)) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) [x, y] = [tempX, tempY];
+    }
+
+    return [x, y];
+}
