669cf8a81306d3797e84726e9deaae574291c966
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-07 21:22:01 +0900

    refactor: 이전 코드로 복구

--- 02.programmers-Lv1/003-달리기경주.ts
+++ 02.programmers-Lv1/003-달리기경주.ts
@@ -11,21 +11,15 @@ export function solution(players: string[], callings: string[]) {
         const calledPlayer = players[backRank];
         const frontPlayer = players[frontRank];
 
-        if (backRank > frontRank) {
-            //첫 호출에서 players를 변경하면 두번째 호출에서 player의 이름 변동 없이 순위만 달라진다.
-            //첫 호출에서 rankedList를 변경하면 calledPlayer와 frontPlayer의 순서가 바뀌어서 해석이 헷갈린다.
-            players[frontRank] = calledPlayer;
-            players[backRank] = frontPlayer;
-        } else {
-            rankedList[calledPlayer] -= 1;
-            rankedList[frontPlayer] += 1;
-        }
+        players[frontRank] = calledPlayer;
+        players[backRank] = frontPlayer;
+        rankedList[calledPlayer] -= 1;
+        rankedList[frontPlayer] += 1;
     };
 
     for (const callName of callings) {
         let backRank = rankedList[callName];
         replacePlayer(backRank, backRank - 1);
-        replacePlayer(backRank - 1, backRank);
     }
 
     return players;
