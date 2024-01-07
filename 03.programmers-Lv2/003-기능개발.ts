d9b66565d8918ca4d2f1ee95e1d985ccfac0ae0f
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-07 15:32:50 +0900

    refactor: 초기값 변경으로 slice 호출 제거

--- 03.programmers-Lv2/003-기능개발.ts
+++ 03.programmers-Lv2/003-기능개발.ts
@@ -6,14 +6,14 @@ export function solution(progresses: number[], speeds: number[]) {
         Math.ceil((100 - v) / speeds[i]),
     );
 
-    let target = requiredDays.splice(0, 1)[0];
-    let count = 1;
+    let target = requiredDays[0];
+    let count = 0;
     for (const day of requiredDays) {
         if (target >= day) count += 1;
         else {
-            answer.push(count); //지금까지 함께 배포 가능한 수 push
+            answer.push(count);
             count = 1;
-            target = day; //비교할 작업시간 교체
+            target = day;
         }
     }
     answer.push(count);
