74b3d77ecaf6a010fefd3a16bda116216d96d059
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-07 21:19:52 +0900

    refactor: arr[-1] 접근 피하기

--- 02.programmers-Lv1/006-같은숫자는싫어.ts
+++ 02.programmers-Lv1/006-같은숫자는싫어.ts
@@ -1,9 +1,10 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12906
 
 export function solution(arr: number[]) {
-    const answer = [];
+    const answer = [arr[0]];
     for (const [i, v] of arr.entries()) {
-        if (v !== arr[i - 1] ?? v) answer.push(v);
+        if (i === 0) continue;
+        if (v !== arr[i - 1]) answer.push(v);
     }
     return answer;
 }
