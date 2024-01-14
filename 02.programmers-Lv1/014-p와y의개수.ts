5ee1e4c103b32ad7ca2f6aa2469fa82b58ee3b73
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-14 19:31:56 +0900

    refactor: 이상한 풀이 개선

--- 02.programmers-Lv1/014-p와y의개수.ts
+++ 02.programmers-Lv1/014-p와y의개수.ts
@@ -1,18 +1,8 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12916
 
-type Char = {
-    [key: string]: number;
-};
-
 export function solution(s: string) {
-    const char: Char = {
-        P: 0,
-        Y: 0,
-    };
-    for (const v of s.toUpperCase()) {
-        if (char[v] !== undefined) char[v] += 1;
-    }
-    return char.P === char.Y ? true : false;
+    const str = s.toUpperCase();
+    return str.split('P').length === str.split('Y').length ? true : false;
 }
 
 //s를 모두 대문자로 변환하기
