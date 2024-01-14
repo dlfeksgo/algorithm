1b463bc2b4d0eb4fbdfc5e66aabb6df5672559a2
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-14 19:30:18 +0900

    refactor: 함수형 코드 개선

--- 02.programmers-Lv1/013-자릿수더하기.ts
+++ 02.programmers-Lv1/013-자릿수더하기.ts
@@ -1,11 +1,7 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12931
 
 export function solution(n: number) {
-    let sum = 0;
-    for (const v of String(n)) {
-        sum += Number(v);
-    }
-    return sum;
+    return Array.from(String(n))
+        .map(Number)
+        .reduce((a, b) => a + b);
 }
-
-//n을 string으로 변환 후 순회하면서 sum 값 누적
