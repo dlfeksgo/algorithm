6c099b5284015f327637b01fad5aa2fd601e08c9
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-15 21:45:27 +0900

    refactor: 중복 제거 함수 작성

--- 03.programmers-Lv2/010-다음큰숫자.ts
+++ 03.programmers-Lv2/010-다음큰숫자.ts
@@ -1,12 +1,13 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12911
+const getLength = (n: number) => {
+    return Array.from(n.toString(2)).filter((v) => v === '1').length;
+};
 
 export function solution(n: number) {
-    const n_leng = Array.from(n.toString(2)).filter((v) => v === '1').length;
+    const n_leng = getLength(n);
     let x = n + 1;
     while (x > n) {
-        const x_leng = Array.from(x.toString(2)).filter(
-            (v) => v === '1',
-        ).length;
+        const x_leng = getLength(x);
         if (n_leng === x_leng) return x;
         x++;
     }
