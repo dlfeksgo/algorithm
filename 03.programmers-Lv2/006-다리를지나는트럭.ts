a892dca42bda2222aa24f3e947bb9338a69a5b99
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-15 22:30:44 +0900

    refactor: 분기 조건 추가

--- 03.programmers-Lv2/006-다리를지나는트럭.ts
+++ 03.programmers-Lv2/006-다리를지나는트럭.ts
@@ -7,24 +7,30 @@ export function solution(
 ) {
     let sec = 0;
     let sum = 0;
-    const trucks = [];
+    const trucks: number[][] = [];
 
     while (truck_weights.length || trucks.length) {
+        const frontTruck = truck_weights[0];
+        //대기중인 트럭이 있다면 올릴 수 있는지 조건 확인 후 가능하면 올리기
         if (
-            weight >= sum + truck_weights[0] &&
+            frontTruck &&
+            weight >= sum + frontTruck &&
             bridge_length >= trucks.length
         ) {
             const truckWeight = truck_weights.shift()!;
             sum += truckWeight;
-
-            //sec + bridge_length: 누적 시간 + 다리 길이 | 올라온 트럭이 다리를 지나는데 걸리는 최소 시간
             trucks.push([truckWeight, sec + bridge_length]);
             sec++;
-        } else {
+        }
+        //대기중인 트럭이 없거나 올릴 수 없으면 다리 위 트럭을 내리기
+        else {
             const [passedTruckWeight, passedSec] = trucks.shift()!;
             if (sec < passedSec) sec = passedSec;
             sum -= passedTruckWeight;
         }
     }
+
     return sec + 1; //마지막 트럭이 빠져나가는 시간 +1 추가
 }
+
+console.log(solution(2, 10, [7, 4, 5, 6]));
