8a41b0f7a617a147991a6d8d6e0337e9ec5aaac5
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-16 23:16:05 +0900

    refactor: for문과 early return

--- 02.programmers-Lv1/021-콜라츠추측.ts
+++ 02.programmers-Lv1/021-콜라츠추측.ts
@@ -1,12 +1,11 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12943
 
 export function solution(num: number) {
-    let i = 0;
     if (num === 1) return 0;
-    while (num !== 1 && i !== 500) {
-        i++;
+    for (let i = 0; i <= 500; i++) {
+        if (num === 1) return i;
         if (num % 2 === 0) num /= 2;
         else num = num * 3 + 1;
     }
-    return i >= 500 ? -1 : i;
+    return -1;
 }
