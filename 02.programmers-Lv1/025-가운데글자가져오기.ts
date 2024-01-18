cf48f5b4524053d083c50e2f65140a8f6b2d6ce6
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-18 22:39:54 +0900

    refactor: slice start 값에 삼항연산자 바로 적용

--- 02.programmers-Lv1/025-가운데글자가져오기.ts
+++ 02.programmers-Lv1/025-가운데글자가져오기.ts
@@ -3,7 +3,5 @@
 export function solution(s: string) {
     const startIdx = Math.floor(s.length / 2);
     const endIdx = startIdx + 1;
-    return s.length % 2 === 0
-        ? s.slice(startIdx - 1, endIdx)
-        : s.slice(startIdx, endIdx);
+    return s.slice(s.length % 2 ? startIdx : startIdx - 1, endIdx);
 }
