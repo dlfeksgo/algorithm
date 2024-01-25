63b1cc8b4a44df6ba809d0e46b1c4ad758741e0d
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-25 18:34:31 +0900

    refactor: 삼총사 문제 entries 적용

--- 02.programmers-Lv1/041-삼총사.ts
+++ 02.programmers-Lv1/041-삼총사.ts
@@ -2,10 +2,12 @@
 
 export function solution(number: number[]) {
     let count = 0;
-    for (let i = 0; i < number.length; i++) {
-        for (let j = i + 1; j < number.length; j++) {
-            for (let k = j + 1; k < number.length; k++) {
-                const sum = number[i] + number[j] + number[k];
+    const student = number.entries();
+    for (const [i, val1] of student) {
+        for (const [j, val2] of student) {
+            for (const [k, val3] of student) {
+                if (j <= i || k <= j) continue;
+                const sum = val1 + val2 + val3;
                 if (sum) continue;
                 count++;
             }
@@ -16,7 +18,7 @@ export function solution(number: number[]) {
 
 console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
 
-//첫 번째 풀이
+//첫 시도
 // let count = 0;
 // while (number.length >= 3) {
 //     const student = number.pop()!;
@@ -29,6 +31,33 @@ console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
 //             target -= number[j];
 //         }
 //         target -= number[i];
+//     }
+// }
+// return count;
+
+//02
+// let count = 0;
+// for (let i = 0; i < number.length; i++) {
+//     for (let j = i + 1; j < number.length; j++) {
+//         for (let k = j + 1; k < number.length; k++) {
+//             const sum = number[i] + number[j] + number[k];
+//             if (sum) continue;
+//             count++;
+//         }
+//     }
+// }
+// return count;
+
+// let count = 0;
+// const student = number.entries();
+// for (const [i, val1] of student) {
+//     for (const [j, val2] of student) {
+//         for (const [k, val3] of student) {
+//             if (j <= i || k <= j) continue;
+//             const sum = val1 + val2 + val3;
+//             if (sum) continue;
+//             count++;
+//         }
 //     }
 // }
 // return count;
