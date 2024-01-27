754771db91cf610fc7aa0dab64b9ed0c17ab4b55
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-27 18:44:33 +0900

    refactor: for문 -> map

--- 02.programmers-Lv1/044-가장가까운같은글자.ts
+++ 02.programmers-Lv1/044-가장가까운같은글자.ts
@@ -1,10 +1,15 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/142086
 
 export function solution(s: string) {
-    const answer: number[] = [];
-    for (let i = 0; i < s.length; i++) {
-        const charIdx = s.slice(0, i).lastIndexOf(s[i], i);
-        answer.push(charIdx > -1 ? i - charIdx : -1);
-    }
-    return answer;
+    return [...s].map((v, i) => {
+        const charIdx = s.slice(0, i).lastIndexOf(v, i);
+        return charIdx > -1 ? i - charIdx : -1;
+    });
 }
+
+// const answer: number[] = [];
+// for (let i = 0; i < s.length; i++) {
+//     const charIdx = s.slice(0, i).lastIndexOf(s[i], i);
+//     answer.push(charIdx > -1 ? i - charIdx : -1);
+// }
+// return answer;
