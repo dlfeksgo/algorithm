8c974a45de17cb8cbbee576b8137f0bb38339307
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-06 18:04:35 +0900

    refactor: isPrimes boolean 배열로 수정

--- 02.programmers-Lv1/046-소수찾기.ts
+++ 02.programmers-Lv1/046-소수찾기.ts
@@ -1,32 +1,34 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
-import _ from 'lodash';
-
 export function solution(n: number) {
-    const numRange = _.range(0, n + 1);
+    const isPrimes = Array(n + 1).fill(true);
+    isPrimes[0] = false;
+    isPrimes[1] = false;
 
     for (let i = 2; i <= Math.sqrt(n); i++) {
         for (let j = i * 2; j <= n; j += i) {
-            if (!numRange[j]) continue;
-            if (numRange[j] % i === 0) numRange[j] = 0;
+            if (!isPrimes[j]) continue;
+            if (j % i === 0) isPrimes[j] = false;
         }
     }
 
-    return numRange.filter((v) => v !== 0).length - 1;
+    return isPrimes.filter((v) => v === true).length;
 }
 
 // export function solution(n: number) {
-//     const set = new Set();
+// const set = new Set();
 
-//     for (let i = 2; i <= Math.sqrt(n); i++) {
-//         for (let j = i * i; j <= n; j += i) {
-//             if (i !== 2 && i % 2 === 0) continue;
-//             if (i !== 3 && i % 3 === 0) continue;
-//             if (i !== 5 && i % 5 === 0) continue;
-//             if (i !== 7 && i % 7 === 0) continue;
-//             if (j % i === 0) set.add(j);
-//         }
+// for (let i = 2; i <= Math.sqrt(n); i++) {
+//     for (let j = i * i; j <= n; j += i) {
+//         if (i !== 2 && i % 2 === 0) continue;
+//         if (i !== 3 && i % 3 === 0) continue;
+//         if (i !== 5 && i % 5 === 0) continue;
+//         if (i !== 7 && i % 7 === 0) continue;
+//         if (j % i === 0) set.add(j);
 //     }
-
-//     return n - set.size - 1;
 // }
+
+// return n - set.size - 1;
+// }
+
+console.log(solution(10));
