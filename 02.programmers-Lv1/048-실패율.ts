a81031da6bc0288b824a2b44228ff7ec068311ad
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-10 01:43:38 +0900

    refactor: 전체 인원수를 적용해 시간복잡도 개선

--- 02.programmers-Lv1/048-실패율.ts
+++ 02.programmers-Lv1/048-실패율.ts
@@ -3,6 +3,7 @@
 export function solution(N: number, stages: number[]) {
     const playersInProgressMap = new Map();
     const failureRate: number[][] = [];
+    let solvedStagePlayers = stages.length;
 
     for (const stage of stages) {
         const val = playersInProgressMap.get(stage) ?? 0;
@@ -11,11 +12,8 @@ export function solution(N: number, stages: number[]) {
 
     for (let i = 1; i <= N; i++) {
         const unresolvedStagePlayers = playersInProgressMap.get(i) ?? 0;
-        let solvedStagePlayers = 0;
-        for (let j = i; j <= N + 1; j++) {
-            solvedStagePlayers += playersInProgressMap.get(j) ?? 0;
-        }
         failureRate.push([i, unresolvedStagePlayers / solvedStagePlayers]);
+        solvedStagePlayers -= unresolvedStagePlayers;
     }
     return failureRate.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
 }
