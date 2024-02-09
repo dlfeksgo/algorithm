4b26a98bc6e490da7916e9f976b6058a3af67a77
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-09 18:15:40 +0900

    refactor: dfs 함수명 변경 & 외부 변수 변경 코드 개선

--- 03.programmers-Lv2/028-최소피로도.ts
+++ 03.programmers-Lv2/028-최소피로도.ts
@@ -1,22 +1,27 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/87946
 
 export function solution(k: number, dungeons: number[][]) {
-    const counts: number[] = [];
     const visited = Array(dungeons.length).fill(false);
-    const dfs = (cnt: number) => {
-        for (const [i, [demand, fatigue]] of dungeons.entries()) {
-            if (!visited[i] && k >= demand) {
+    let maxDungeons = 0;
+
+    const recursiveDungeonExplore = (remainingFatigue: number, count: number) => {
+        maxDungeons = Math.max(maxDungeons, count);
+        for (const [i, [requiredFatigue, consumedFatigue]] of dungeons.entries()) {
+            if (!visited[i] && remainingFatigue >= requiredFatigue) {
                 visited[i] = true;
-                k -= fatigue;
-                counts.push(cnt + 1); //if문 내로 진입하면 탐험이 가능하므로 cnt를 +1 해준다.
-                dfs(cnt + 1);
-                //dfs 재귀호출 내 for...of 순회가 끝나면 마지막 호출 시점의 idx 순회로 돌아온다.
-                //A - B - C 순서로 탐색한 후 A - C - B 순서로 순회가 가능해진다.
+                recursiveDungeonExplore(remainingFatigue - consumedFatigue, count + 1);
                 visited[i] = false;
-                k += fatigue;
             }
         }
     };
-    dfs(0);
-    return Math.max(...counts);
+    recursiveDungeonExplore(k, 0);
+    return maxDungeons;
 }
+
+console.log(
+    solution(80, [
+        [80, 20],
+        [50, 40],
+        [30, 10],
+    ]),
+);
