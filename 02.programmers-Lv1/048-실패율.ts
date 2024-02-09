74c2fab5f2ce48dad6ad002831e669cd853c785b
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-09 18:15:08 +0900

    refactor: 실패율 문제 2번째 filter 개선

--- 02.programmers-Lv1/048-실패율.ts
+++ 02.programmers-Lv1/048-실패율.ts
@@ -1,20 +1,23 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42889
 
 export function solution(N: number, stages: number[]) {
-    const stageMap = new Map();
+    const playersInProgressMap = new Map();
     const failureRate: number[][] = [];
 
-    for (const num of stages) {
-        const val = stageMap.get(num) ?? 0;
-        stageMap.set(num, val + 1);
+    for (const stage of stages) {
+        const val = playersInProgressMap.get(stage) ?? 0;
+        playersInProgressMap.set(stage, val + 1);
     }
-    for (let i = 1; i <= N; i++) {
-        const players = stages.filter((v) => v >= i).length;
-        const stage = stageMap.get(i) ?? 0;
 
-        failureRate.push([i, stage / players]);
+    for (let i = 1; i <= N; i++) {
+        const unresolvedStagePlayers = playersInProgressMap.get(i) ?? 0;
+        let solvedStagePlayers = 0;
+        for (let j = i; j <= N + 1; j++) {
+            solvedStagePlayers += playersInProgressMap.get(j) ?? 0;
+        }
+        failureRate.push([i, unresolvedStagePlayers / solvedStagePlayers]);
     }
     return failureRate.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
 }
 
-console.log(solution(4, [4, 4, 4, 4, 4]));
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
