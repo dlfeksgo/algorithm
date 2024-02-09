941b96cd15203814c190a7b3523dc2598fe4d6ac
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-09 18:33:25 +0900

    refactor: if문 병합

--- 02.programmers-Lv1/046-소수찾기.ts
+++ 02.programmers-Lv1/046-소수찾기.ts
@@ -7,25 +7,9 @@ export function solution(n: number) {
 
     for (let i = 2; i <= Math.sqrt(n); i++) {
         for (let j = i * i; j <= n; j += i) {
-            if (!isPrimes[j]) continue;
-            if (j % i === 0) isPrimes[j] = false;
+            if (isPrimes[j] && j % i === 0) isPrimes[j] = false;
         }
     }
 
     return isPrimes.filter((v) => v === true).length;
 }
-// export function solution(n: number) {
-// const set = new Set();
-
-// for (let i = 2; i <= Math.sqrt(n); i++) {
-//     for (let j = i * i; j <= n; j += i) {
-//         if (i !== 2 && i % 2 === 0) continue;
-//         if (i !== 3 && i % 3 === 0) continue;
-//         if (i !== 5 && i % 5 === 0) continue;
-//         if (i !== 7 && i % 7 === 0) continue;
-//         if (j % i === 0) set.add(j);
-//     }
-// }
-
-// return n - set.size - 1;
-// }
