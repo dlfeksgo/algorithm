0bbf9ceb4f1c0f5fa7d27467337a72d112aa9080
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-14 16:17:45 +0900

    refactor: dfs 함수명 수정

--- 00.baekjoon/039-1240.ts
+++ 00.baekjoon/039-1240.ts
@@ -1,10 +1,6 @@
 //https://www.acmicpc.net/problem/1240
 
-const input: string[] = require('fs')
-    .readFileSync('input.txt')
-    .toString()
-    .trim()
-    .split('\n');
+const input: string[] = require('fs').readFileSync('input.txt').toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
 const links: number[][][] = Array.from(Array(N + 1), () => []);
@@ -17,20 +13,20 @@ for (let i = 1; i <= N - 1; i++) {
     links[y].push([x, point]);
 }
 
-const dfs = (start: number, end: number, dist: number) => {
+const findPath = (start: number, end: number, dist: number) => {
     if (start === end) {
         return (count = dist);
     }
     if (visited[start]) return;
     visited[start] = true;
     for (const [y, point] of links[start]) {
-        dfs(y, end, dist + point);
+        findPath(y, end, dist + point);
     }
 };
 
 for (let i = N; i < N + M; i++) {
     const [start, end] = input[i].split(' ').map(Number);
     visited = Array(N + 1).fill(false);
-    dfs(start, end, 0);
+    findPath(start, end, 0);
     console.log(count);
 }
