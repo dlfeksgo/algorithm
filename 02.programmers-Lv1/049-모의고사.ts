d0dfc41ece441b7083bc3f20a8095ef0d504091d
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-04-01 12:29:53 +0900

    refactor: 불필요한 함수 제거 및 비효율 개선

--- 02.programmers-Lv1/049-모의고사.ts
+++ 02.programmers-Lv1/049-모의고사.ts
@@ -1,25 +1,17 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42840
 
-const getScoreByStudents = (answers: number[], students: Record<string, number[]>) => {
-    const result: number[] = [];
-    for (const [i, key] of Object.keys(students).entries()) {
-        result[i] = answers.filter(
-            (answer, idx: number) => answer === students[key][idx % students[key].length],
-        ).length;
-    }
-    return result;
-};
-
 export function solution(answers: number[]) {
-    const students: Record<string, number[]> = {
-        one: [1, 2, 3, 4, 5],
-        two: [2, 1, 2, 3, 2, 4, 2, 5],
-        three: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-    };
-
-    const scoreByStudents = getScoreByStudents(answers, students);
-    const maxScore = Math.max(...scoreByStudents);
-    return scoreByStudents.map((v, i) => (maxScore === v ? i + 1 : -1)).filter((v) => v > 0);
+    const students: number[][] = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+    ];
+    const score = [0, 0, 0];
+    for (const [i, answer] of answers.entries()) {
+        for (const [j, student] of students.entries()) {
+            if (answer === student[i % student.length]) score[j]++;
+        }
+    }
+    const maxScore = Math.max(...score);
+    return score.map((v, i) => (maxScore === v ? i + 1 : -1)).filter((v) => v > 0);
 }
-
-console.log(solution([1, 2, 3, 4, 5]));
