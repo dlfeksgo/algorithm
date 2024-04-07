014cbd1e4b4bcdcc8c60b605e1fb71ff1872f268
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-04-08 00:39:33 +0900

    refactor: 반환값을 갖지 않는 DFS 함수로 개선

--- 04.programmers-Lv3/002-네트워크.ts
+++ 04.programmers-Lv3/002-네트워크.ts
@@ -1,22 +1,25 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/43162
 
-function getNetworkCnt(n: number, computers: number[][]) {
+function getNetworkCnt(n: number, computers: number[][]): number {
     let network = 0;
     const visited: number[] = Array(n).fill(0);
 
-    const isNetworkConnected = (comIdx: number) => {
-        if (visited[comIdx]) return 0;
+    const checkLinkedNetwork = (comIdx: number) => {
+        if (visited[comIdx]) return;
         visited[comIdx] = 1;
 
         for (const linkIdx of computers.keys()) {
-            if (computers[comIdx][linkIdx]) isNetworkConnected(linkIdx);
+            if (computers[comIdx][linkIdx]) checkLinkedNetwork(linkIdx);
         }
-        return 1;
     };
 
-    for (const i of computers.keys()) {
-        network += isNetworkConnected(i);
+    for (const comIdx of computers.keys()) {
+        if (!visited[comIdx]) {
+            checkLinkedNetwork(comIdx);
+            network++;
+        }
     }
+
     return network;
 }
 
@@ -27,3 +30,24 @@ console.log(
         [0, 1, 1],
     ]),
 );
+
+// 값 반환 풀이
+// function getNetworkCnt(n: number, computers: number[][]) {
+//     let network = 0;
+//     const visited: number[] = Array(n).fill(0);
+
+//     const isNetworkConnected = (comIdx: number) => {
+//         if (visited[comIdx]) return 0;
+//         visited[comIdx] = 1;
+
+//         for (const linkIdx of computers.keys()) {
+//             if (computers[comIdx][linkIdx]) isNetworkConnected(linkIdx);
+//         }
+//         return 1;
+//     };
+
+//     for (const i of computers.keys()) {
+//         network += isNetworkConnected(i);
+//     }
+//     return network;
+// }
