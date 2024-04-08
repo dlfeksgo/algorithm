06afef3defc5fd458600811434ee7682da3fe6e0
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-04-08 11:17:31 +0900

    refactor: DFS 함수명 수정

--- LeetCode/207-CourseSchedule.ts
+++ LeetCode/207-CourseSchedule.ts
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/course-schedule/description/
 
-//사이클이 존재하면 안된다.(순환이 되면 안된다)
+//Topological Sort
 export function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const adjacencyList: number[][] = Array.from({ length: numCourses }, () => []);
     const incomingEdges: number[] = Array(numCourses).fill(0);
@@ -14,13 +14,13 @@ export function canFinish(numCourses: number, prerequisites: number[][]): boolea
     let coursesTaken = 0;
 
     //선행 강의가 없다는 것은 바로 수강이 가능하다는 뜻이다.
-    function canCourseProceed(course: number) {
+    function tryCourseProceed(course: number) {
         if (!incomingEdges[course]) queue.push(course);
     }
 
     //for...of문 대체로 course idx로만 체크
     for (let i = 0; i < numCourses; i++) {
-        canCourseProceed(i);
+        tryCourseProceed(i);
     }
 
     while (queue.length) {
@@ -28,7 +28,7 @@ export function canFinish(numCourses: number, prerequisites: number[][]): boolea
         coursesTaken++;
 
         for (const pre of adjacencyList[currentCourse]) {
-            if (!--incomingEdges[pre]) canCourseProceed(pre);
+            if (!--incomingEdges[pre]) tryCourseProceed(pre);
         }
     }
     return coursesTaken === numCourses;
