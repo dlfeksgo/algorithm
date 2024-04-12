323760365bee43ce0be49209c01e6b62d6cbbf28
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-04-13 02:45:02 +0900

    refactor: find와 union 함수 클로저로 적용

--- 04.programmers-Lv3/003-섬연결하기.ts
+++ 04.programmers-Lv3/003-섬연결하기.ts
@@ -1,21 +1,6 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42861
 
 //Kruskal
-const find = (x: number, parents: number[]): number => {
-    if (x !== parents[x]) {
-        parents[x] = find(parents[x], parents);
-    }
-    return parents[x];
-};
-
-const union = (x: number, y: number, parents: number[]): boolean => {
-    if (x < y) {
-        parents[y] = x;
-    } else {
-        parents[x] = y;
-    }
-    return true;
-};
 
 type Edge = [number, number, number];
 const getMinCost = (n: number, costs: Edge[]): number => {
@@ -25,18 +10,35 @@ const getMinCost = (n: number, costs: Edge[]): number => {
 
     let totalCost = 0;
 
+    const find = (x: number): number => {
+        if (x !== parents[x]) {
+            parents[x] = find(parents[x]);
+        }
+        return parents[x];
+    };
+
+    const union = (x: number, y: number): boolean => {
+        if (x < y) {
+            parents[y] = x;
+        } else {
+            parents[x] = y;
+        }
+        return true;
+    };
+
     const findParentNode = (x: number, y: number) => {
-        const parentX = find(x, parents);
-        const parentY = x === y ? parentX : find(y, parents);
+        const parentX = find(x);
+        const parentY = x === y ? parentX : find(y);
         return [parentX, parentY];
     };
+
     // 3. 모든 간선을 순회하며 최소 신장 트리 만들기
     for (const [nodeX, nodeY, cost] of costs) {
         const [parentX, parentY] = findParentNode(nodeX, nodeY);
 
         if (parentX === parentY) continue; //사이클 발생
 
-        union(parentX, parentY, parents);
+        union(parentX, parentY);
         totalCost += cost;
     }
 
