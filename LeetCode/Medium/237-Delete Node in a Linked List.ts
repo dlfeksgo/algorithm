f34da6036c873060506fed97eb92e943df6c8448
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-17 20:17:33 +0900

    feat: [Medium] 1문제 풀이

---
+++ LeetCode/Medium/237-Delete Node in a Linked List.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+export function deleteNode(node: ListNode | null): void {
+  node.val = node.next;
+  node.next = node.next.next;
+}
