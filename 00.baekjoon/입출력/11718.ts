70f5277de713f0bc3fcc2f74512576ad2eff8761
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-01-02 23:12:02 +0900

    feat: [Bronze3] 2문제 풀이

---
+++ 00.baekjoon/입출력/11718.ts
@@ -0,0 +1,4 @@
+// https://www.acmicpc.net/problem/11718
+
+const input = require('fs').readFileSync('input.txt').toString();
+console.log(input);
---
+++ 00.baekjoon/입출력/11719.ts
@@ -0,0 +1,4 @@
+//https://www.acmicpc.net/problem/11719
+
+const input = require('fs').readFileSync('input.txt').toString();
+console.log(input);
